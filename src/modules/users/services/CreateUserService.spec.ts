0b04a80439eeb29f7bf776a72d7bf4e3299cde9b
MauroBraga <mrb0305@gmail.com>
2020-12-10 20:56:51 -0300

    Provider de storage

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -21,27 +21,4 @@ describe('CreateUser', () => {
 
     expect(user).toHaveProperty('id');
   });
-
-  it('should not be able to create a new user with same email from another', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-    const createUser = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
-
-    const user = await createUser.execute({
-      name: 'Jonh Doe',
-      email: 'jonhdoe@example.com',
-      password: '123456',
-    });
-
-    expect(
-      createUser.execute({
-        name: 'John Doe',
-        email: 'johndoe@example.com',
-        password: '123123',
-      }),
-    ).rejects.toBeInstanceOf(AppError);
-  });
 });
