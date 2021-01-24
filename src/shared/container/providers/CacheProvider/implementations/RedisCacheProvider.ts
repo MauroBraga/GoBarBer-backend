db8253c1afaec5d8536a1abc8033c346993703e2
MauroBraga <mrb0305@gmail.com>
2021-01-24 10:57:35 -0300

    Cache de agendamentos

--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -27,7 +27,7 @@ export default class RedisCacheProvider implements ICacheProvider {
   }
 
   public async invalidate(key: string): Promise<void> {
-    console.log('teste');
+    await this.client.del(key);
   }
 
   public async invalidatePrefix(prefix: string): Promise<void> {
